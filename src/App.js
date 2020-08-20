3d428ce9bf03c61994cb433be8fcc48ab5fc5a75
Johnny Li <reptile18@gmail.com>
2020-08-19 19:39:26 -0700

    try basename pub url again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       {console.log(`process.env.PUBLIC_URL: ${process.env.PUBLIC_URL}`)}
       <div className="App">
         <Navbar></Navbar>
