42161f6f1309b8d0d0109ca85e56dc136e3f9aed
Johnny Li <reptile18@gmail.com>
2020-08-19 19:29:42 -0700

    Updates

--- precache-manifest.0f8129980aeecef36d36c1b928bae5ab.js
+++ precache-manifest.0f8129980aeecef36d36c1b928bae5ab.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b76efe4683d9eb79f5b9fbaa31347f12",
+    "revision": "0fa0fe4e6856749d98c2ca39fe25ad6f",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "25fac803ec44f1810b7b",
+    "revision": "0b87a18c84b53a11edf2",
     "url": "/react-portfolio/static/css/main.09c87c91.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.1632d3de.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "25fac803ec44f1810b7b",
-    "url": "/react-portfolio/static/js/main.b5c0f4cf.chunk.js"
+    "revision": "0b87a18c84b53a11edf2",
+    "url": "/react-portfolio/static/js/main.64751e77.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
