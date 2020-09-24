4fc13683fc8c39f4701c7593a25e86a888c945a4
Johnny Li <reptile18@gmail.com>
2020-09-23 17:04:54 -0700

    Updates

---
+++ precache-manifest.ca050d041202e47301d9c98cee60683f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd94c5cf5175192fbb71d89254b5c2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "932957f166cb2c8f6e20",
+    "url": "/static/css/main.57143a70.chunk.css"
+  },
+  {
+    "revision": "5de07e4f68071724806c",
+    "url": "/static/js/2.dec2b25d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dec2b25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932957f166cb2c8f6e20",
+    "url": "/static/js/main.8caeb3b5.chunk.js"
+  },
+  {
+    "revision": "a653cec2941d42158079",
+    "url": "/static/js/runtime-main.c6b353fb.js"
+  },
+  {
+    "revision": "fba5f844010e3e0031358c4f35b90d71",
+    "url": "/static/media/brick.fba5f844.png"
+  },
+  {
+    "revision": "f24a8485d81ea3fbb20b58001163a29d",
+    "url": "/static/media/cellphones-min.f24a8485.jpg"
+  },
+  {
+    "revision": "3d0d31c156a1cfd00f4f7af5aae75063",
+    "url": "/static/media/codingbackground-min.3d0d31c1.png"
+  },
+  {
+    "revision": "4ce1fffe2886fbb8c6798163126ee9ef",
+    "url": "/static/media/concrete-3-min.4ce1fffe.jpg"
+  },
+  {
+    "revision": "f47455326884dd5a08aa8ac250f2ff57",
+    "url": "/static/media/interface0.f4745532.png"
+  },
+  {
+    "revision": "758b033a67358583b0291f45320c1206",
+    "url": "/static/media/interface1.758b033a.png"
+  },
+  {
+    "revision": "8b83b4bd99486e378c3c93388f945ffd",
+    "url": "/static/media/interface2.8b83b4bd.png"
+  },
+  {
+    "revision": "71d8e035f7cca4e7ba020039a89f1d3e",
+    "url": "/static/media/interfaces.71d8e035.jpg"
+  },
+  {
+    "revision": "9f35121ba5e2aa76fd9d8930d88a6815",
+    "url": "/static/media/lighttunnel.9f35121b.jpeg"
+  },
+  {
+    "revision": "98b3e37a19b99dbd9746b48271834ad0",
+    "url": "/static/media/pg1.98b3e37a.png"
+  },
+  {
+    "revision": "a4e7e8e697ba20d28b093c214a144f8a",
+    "url": "/static/media/pg2.a4e7e8e6.png"
+  },
+  {
+    "revision": "74ebbfe3962cbc05ffb388fc20e64f60",
+    "url": "/static/media/pixel2_ccidi.74ebbfe3.png"
+  },
+  {
+    "revision": "719c3520631158ece2dba90a5af72fa9",
+    "url": "/static/media/pixel2_sttl.719c3520.png"
+  },
+  {
+    "revision": "8c7ba39d4f6e11df900f8846b31daeed",
+    "url": "/static/media/professionaljohnny.8c7ba39d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.f0ec21fdea761a80b245cb5d211607cf.js"
+  "/precache-manifest.ca050d041202e47301d9c98cee60683f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
